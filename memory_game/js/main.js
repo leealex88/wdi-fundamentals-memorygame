d58e43492e07489b7c6deab1b5227ae07e164e97
Ola <aleksandra@ALEKSANDRAs-MacBook-Air.local>
2019-04-28 20:15:01 +0100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,18 @@
+console.log("Up and running!");
+
+
+var cards = ["queen", "queen", "king","king"];
+var cardsInPlay = [];
+var CardOne = [0];
+cardsInPlay.push('cardOne');
+console.log("User flipped Queen");
+
+var CardTwo =[2]
+cardsInPlay.push('cardTwo');
+
+var cardsInPlay
+
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[2]) { alert("is working"); }
+else { alert ("try later."); }
+} 
