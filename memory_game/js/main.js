5df19e071dc07ddd5a111831c1cf6600102c00a2
Ola <aleksandra@ALEKSANDRAs-MacBook-Air.local>
2019-05-02 13:17:45 +0200

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,16 +3,32 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king","king"];
 var cardsInPlay = [];
-var CardOne = [0];
-cardsInPlay.push('cardOne');
-console.log("User flipped Queen");
 
-var CardTwo =[2]
-cardsInPlay.push('cardTwo');
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
 
-var cardsInPlay
-
+function flipCard(cardId){
+console.log("User flipped" + " " + cards[cardId]);
 if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[2]) { alert("is working"); }
-else { alert ("try later."); }
+	checkForMatch()
+}
+{if (cardsInPlay[0] === cardsInPlay[2]) 
+	{alert("You found a match"); }
+else { alert ("Sorry try again"); }
 } 
+}
+flipCard(0);
+/*var CardOne = [0];
+cardsInPlay.push('cardOne');
+console.log("User flipped" + cardOne");*/
+
+/*var CardTwo =[2]
+cardsInPlay.push('cardTwo');
+console.log("User flipped" + cardTwo);
+*/
+
